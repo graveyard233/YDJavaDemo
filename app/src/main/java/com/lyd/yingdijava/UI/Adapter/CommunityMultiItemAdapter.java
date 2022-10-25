703674b76a30c2d208d62ab4c538f52e25e40f94
graveyard233 <382287750@qq.com>
2022-10-25 17:21:32 +0800

    多布局adapter初步逻辑完成 需要新增layout来继续实现瀑布流多布局

--- app/src/main/java/com/lyd/yingdijava/UI/Adapter/CommunityMultiItemAdapter.java
+++ app/src/main/java/com/lyd/yingdijava/UI/Adapter/CommunityMultiItemAdapter.java
@@ -1,27 +1,95 @@
 package com.lyd.yingdijava.UI.Adapter;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseMultiItemAdapter;
+import com.google.android.material.card.MaterialCardView;
+import com.lyd.yingdijava.Entity.Community.BaseCommunityNode;
 import com.lyd.yingdijava.Entity.Community.CommunityPostNode;
+import com.lyd.yingdijava.R;
+
+import java.util.List;
 
 // TODO: 2022/10/23 准备测试多布局 
 public class CommunityMultiItemAdapter extends BaseMultiItemAdapter<CommunityPostNode> {
 
-    protected static class RoutineVH extends RecyclerView.ViewHolder{
+    public CommunityMultiItemAdapter(@NonNull List<? extends CommunityPostNode> items) {
+        super(items);
+        OnMultiItemAdapterListener<CommunityPostNode,RoutineVH> listenerRoutine = new OnMultiItemAdapterListener<CommunityPostNode, RoutineVH>() {
+            @NonNull
+            @Override
+            public RoutineVH onCreate(@NonNull Context context, @NonNull ViewGroup viewGroup, int i) {
+                return new RoutineVH(LayoutInflater.from(context).inflate(R.layout.item_news_list,viewGroup,false));
+            }
 
+            @Override
+            public void onBind(@NonNull RoutineVH routineVH, int i, @Nullable CommunityPostNode communityPostNode) {
+                routineVH.title.setText(communityPostNode.getTitle());
+                routineVH.reply.setText(communityPostNode.getPostType().toString());
+                routineVH.img.setBackgroundColor(Color.BLUE);
+            }
+        };
+        OnMultiItemAdapterListener<CommunityPostNode, ArticleVH> listenerArticle = new OnMultiItemAdapterListener<CommunityPostNode, ArticleVH>() {
+            @NonNull
+            @Override
+            public ArticleVH onCreate(@NonNull Context context, @NonNull ViewGroup viewGroup, int i) {
+                return new ArticleVH(LayoutInflater.from(context).inflate(R.layout.item_news_list,viewGroup,false));
+            }
+
+            @Override
+            public void onBind(@NonNull ArticleVH articleVH, int i, @Nullable CommunityPostNode communityPostNode) {
+                articleVH.title.setText(communityPostNode.getTitle());
+                articleVH.reply.setText(communityPostNode.getPostType().toString());
+                articleVH.img.setBackgroundColor(Color.GREEN);
+            }
+        };
+        addItemType(0,RoutineVH.class,listenerRoutine);
+        addItemType(1,ArticleVH.class,listenerArticle);
+        onItemViewType(new OnItemViewTypeListener<CommunityPostNode>() {
+            @Override
+            public int onItemViewType(int i, @NonNull List<? extends CommunityPostNode> list) {
+                switch (list.get(i).getPostType()){
+                    case ArticlePost:
+                        return 1;
+                    default:
+                        return 0;
+                }
+//                return 0;
+            }
+        });
+    }
+
+    protected class RoutineVH extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView reply;
+        ImageView img;
         public RoutineVH(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.item_news_title);
+            reply = itemView.findViewById(R.id.item_news_reply);
+            img = itemView.findViewById(R.id.item_news_img);
         }
     }
 
-    protected static class ArticlePost extends RecyclerView.ViewHolder{
-        
-        public ArticlePost(@NonNull View itemView) {
+    protected class ArticleVH extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView reply;
+        ImageView img;
+        public ArticleVH(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.item_news_title);
+            reply = itemView.findViewById(R.id.item_news_reply);
+            img = itemView.findViewById(R.id.item_news_img);
         }
     }
 }
--- app/src/main/java/com/lyd/yingdijava/UI/Fragment/CommunityFragment.java
+++ app/src/main/java/com/lyd/yingdijava/UI/Fragment/CommunityFragment.java
@@ -5,10 +5,12 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lyd.yingdijava.Entity.Community.CommunityPostNode;
+import com.lyd.yingdijava.UI.Adapter.CommunityMultiItemAdapter;
 import com.lyd.yingdijava.UI.Base.BaseFragment;
 import com.lyd.yingdijava.R;
 import com.lyd.yingdijava.ViewModel.MessageViewModel;
@@ -37,6 +39,7 @@ public class CommunityFragment extends BaseFragment {
         initViewModel();
         observeLiveData();
 
+        recyclerView = find(R.id.community_recyclerView);
         refreshLayout = find(R.id.community_refresh_layout);
         refreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
@@ -63,6 +66,11 @@ public class CommunityFragment extends BaseFragment {
                         communityPostNodes) {
                     Log.i(TAG, "Type: " + node.getPostType().toString() + ",Title: " + node.getTitle());
                 }
+                CommunityMultiItemAdapter adapter = new CommunityMultiItemAdapter(communityPostNodes);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+                refreshLayout.finishRefresh(true);
             }
         });
         messageViewModel.getCommunityPostErrorLiveData().observe(CommunityFragment.this, new Observer<String>() {
